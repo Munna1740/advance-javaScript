00d9e258b6325df80e370fb984083e83de3b8a4e
Munna1740 <mdmostafamunna@gmail.com>
2020-03-28 01:12:58 +0600

    scope file added

---
+++ scope.js
@@ -0,0 +1,8 @@
+function sum(num1,num2){
+
+    const result= num1+num2;
+    return result;
+}
+const output = sum(10,10);
+console.log(output);
+console.log(result); // its not a global variable...if we declare var then it hoisting ta variable in up...and its scope in the parents access area...
