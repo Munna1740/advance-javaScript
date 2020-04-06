db57cb2c102609e9e12d88bfd137c517c8a06c31
Munna1740 <mdmostafamunna@gmail.com>
2020-04-07 00:08:56 +0600

    Templete create

--- scope.js
+++ scope.js
@@ -6,3 +6,4 @@ function sum(num1,num2){
 const output = sum(10,10);
 console.log(output);
 console.log(result); // its not a global variable...if we declare var then it hoisting ta variable in up...and its scope in the parents access area...
+
